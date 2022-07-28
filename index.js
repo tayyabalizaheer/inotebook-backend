6434652ed71fe62600b62780d8c77a8107286f49
whoisyamyam <mariamalnuaimi98@gmail.com>
2022-07-28 13:13:24 +0400

    check

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const express = require('express');
-const connectToMongo = require('./db');
+// const connectToMongo = require('./db');
 const multer = require('multer');
-var forms = multer();
+// var forms = multer();
 const app = express();
-const port = 3001;
+const port = 8081;
 
-connectToMongo();
+// connectToMongo();
 
-//Routes
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(forms.array());
-app.use('/api/auth', require('./routes/auth'));
+// //Routes
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(forms.array());
+// app.use('/api/auth', require('./routes/auth'));
 // app.use('/api/note', require('./routes/note'));
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -20,3 +20,12 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Example app listening on port ${port} and link http://localhost:${port}`);
 })
+
+
+// // PORT
+// const PORT = process.env.PORT || 8081;
+
+// // Listen on port 8081
+// app.listen(PORT, () =>
+//     console.log(`Application is listening on port ${PORT}!`)
+// );
