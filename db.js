122f824677d6b8e215da91ee1e1e09de7eef2abb
whoisyamyam <mariamalnuaimi98@gmail.com>
2022-07-28 13:57:31 +0400

    db change

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoUrl = "mongodb://localhost:27017/inotebook";
+const mongoUrl = "mongodb+srv://tayyabalizaheer:IYYtvWxAw4SPMbSl@inotebook.nq3ge1m.mongodb.net/?retryWrites=true&w=majority";
 
 const connectToMongo = () => {
     mongoose.connect(mongoUrl, ()=>{
