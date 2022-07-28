27c8b1024faf7211099217c110166786d997db81
whoisyamyam <mariamalnuaimi98@gmail.com>
2022-07-28 13:25:15 +0400

    test

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const express = require('express');
-// const connectToMongo = require('./db');
+const connectToMongo = require('./db');
 const multer = require('multer');
-// var forms = multer();
+var forms = multer();
 const app = express();
-const port = 8081;
+const port = process.env.port || 8081;
 
-// connectToMongo();
+connectToMongo();
 
-// //Routes
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(forms.array());
-// app.use('/api/auth', require('./routes/auth'));
+//Routes
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(forms.array());
+app.use('/api/auth', require('./routes/auth'));
 // app.use('/api/note', require('./routes/note'));
 app.get('/', (req, res) => {
     res.send('Hello World!')
